0e182b8b1c680337a43fb4521f516307905e9782
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-11-13 21:17:07 +0200

    added 2 lines - updated

--- src/Car.java
+++ src/Car.java
@@ -9,6 +9,6 @@ public class Car {
     public static void main(String[] args) {
         System.out.println(5 / 0);
         System.out.println(5 / 0);
-        System.out.println(5 / 0);
+        System.out.println(7 / 0);
     }
 }
